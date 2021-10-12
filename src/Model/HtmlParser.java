8e5d3d48fe9671c318690b0093f7229d1d99f75e
Joaquín Jiménez <joaquinenrique2010@gmail.com>
2021-10-12 17:23:02 -0600

    C: HtmlParser class

---
+++ src/Model/HtmlParser.java
@@ -0,0 +1,32 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class HtmlParser {
+
+	public static String getBodyText(String html) {
+		
+		return "";
+	}
+	
+	public static String geTitleText(String html) {
+		
+		return "";
+	}
+		
+	public static String getATagsText(String html) {
+		
+		return "";
+	}
+
+	public static String getHeadersText(String html) {
+		
+		return "";
+	}
+	
+public static ArrayList<String> getLinks(String html) {
+		ArrayList<String> links = new ArrayList<>();
+		
+		return links;
+	}
+}
